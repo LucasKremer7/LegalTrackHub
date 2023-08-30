9f8f879a4b37467e47b38450779bec6920af7e8a
LucasKremer <lucas.kremer7@gmail.com>
2023-08-29 21:06:02 -0300

    L.K | 29/08/2023 21:06 | Criada a classe de consulta e funções de start. Adicionado a pasta testes com as planilhas de testes.

--- index.py
+++ index.py
@@ -2,17 +2,41 @@ from selenium import webdriver
 from time import sleep
 from dotenv import load_dotenv
 import os
+import pandas as pd
+import openpyxl
 
 load_dotenv()
 
-usuario = os.getenv('USUARIO')
-print(usuario)
-senha = os.getenv('SENHA')
-print(senha)
 
-options = webdriver.ChromeOptions()
+class Consulting():
 
-driver = webdriver.Chrome(options=options)
+    def __init__(self):
+        self.usuario = os.getenv('USUARIO')
+        self.senha = os.getenv('SENHA')
+        self.base_path = os.getenv('BASE_PATH')
+        self.options = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(options=self.options)
+        self.sep = self.sep = '/' if eval(os.getenv('IS_LINUX')) else '\\'
+        self.options.add_argument("--headless")
 
-url = driver.get('https://www.selenium.dev/documentation/webdriver/getting_started/first_script/')
-sleep(5)
+    def start(self):
+        lista_cnpj = []
+        planilha = pd.read_excel(f"{self.base_path}{self.sep}testes{self.sep}teste_pre_dot.xlsx")
+        df = pd.DataFrame(planilha)
+        sleep(0.1)
+        
+        for index, valor in df:
+            print(index)
+            print(valor)
+            lista_cnpj.append(valor)
+
+    def scraping(self):
+        url = self.driver.get('https://www.selenium.dev/documentation/webdriver/getting_started/first_script/')
+        self.driver.maximize_window()
+        sleep(5)
+
+if __name__ == "__main__":
+
+    app = Consulting()
+    app.start()
+    print('Código finalizado com sucesso!')
